848e031275bfc4865d07a6ee02944f1bc0f61174
Kriegcho <1002310519@qq.com>
2022-02-16 13:38:40 +0800

    0007_Reverse Integer

---
+++ LeetCode/Code/0007_Reverse Integer.cpp
@@ -0,0 +1,21 @@
+﻿#include <cmath>
+
+class Solution {
+public:
+	int reverse(int x) {
+		long ans = 0;
+		int d = 0, t = x;
+		while (t)
+		{
+			d++;
+			t /= 10;
+		}
+		for (int i = 1; i <= d; i++)
+		{
+			ans += (x % 10) * pow(10, d - i);
+			x /= 10;
+		}
+		if (ans >= -pow(2, 31) && ans < pow(2, 31)) return ans;
+		else return 0;
+	}
+};
