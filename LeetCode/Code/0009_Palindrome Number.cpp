75bd32d889838bd1dcdb078cab9588d9b1551761
Kriegcho <1002310519@qq.com>
2022-02-16 13:44:39 +0800

    0009_Palindrome Number

---
+++ LeetCode/Code/0009_Palindrome Number.cpp
@@ -0,0 +1,20 @@
+﻿#include <cmath>
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		if (x < 0) return false;
+		long tx = x, temp = x, ty = 0, d = 0;
+		while (x)
+		{
+			d++;
+			x /= 10;
+		}
+		for (int i = 1; i <= d; i++)
+		{
+			ty += (temp % 10) * pow(10, d - i);
+			temp /= 10;
+		}
+		return tx == ty;
+	}
+};
