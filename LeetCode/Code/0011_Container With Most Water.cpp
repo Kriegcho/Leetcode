3b5e63dfce74fa96caffff925faa6602c01a4f2a
Kriegcho <1002310519@qq.com>
2022-02-17 11:46:02 +0800

    0011_Container With Most Water。时间复杂度O(N)。

--- LeetCode/Code/0011_Container With Most Water.cpp
+++ LeetCode/Code/0011_Container With Most Water.cpp
@@ -4,30 +4,48 @@ using namespace std;
 
 class Solution {
 public:
-	int maxArea(vector<int>& height) { // 复杂度O(n²)，通过剪枝勉强ac
+	//int maxArea(vector<int>& height) { // 复杂度O(n²)，通过剪枝勉强ac
+	//	int ans = 0, n = height.size();
+	//	for (int i = 0; i < n; i++)
+	//	{
+	//		for (int j = n - 1; j > i; j--)
+	//		{
+	//			if (height[j] >= height[i])
+	//			{
+	//				ans = max(ans, height[i] * (j - i));
+	//				break;
+	//			}
+	//			else if (ans >= height[i] * (j - i)) break;
+	//		}
+	//	}
+	//	for (int i = n - 1; i >= 0; i--)
+	//	{
+	//		for (int j = 0; j < i; j++)
+	//		{
+	//			if (height[j] >= height[i])
+	//			{
+	//				ans = max(ans, height[i] * (i - j));
+	//				break;
+	//			}
+	//			else if (ans >= height[i] * (i - j)) break;
+	//		}
+	//	}
+	//	return ans;
+	//}
+	int maxArea(vector<int>& height) {
 		int ans = 0, n = height.size();
-		for (int i = 0; i < n; i++)
+		int l = 0, h = n - 1;
+		while (l < h)
 		{
-			for (int j = n - 1; j > i; j--)
+			if (height[l] < height[h])
 			{
-				if (height[j] >= height[i])
-				{
-					ans = max(ans, height[i] * (j - i));
-					break;
-				}
-				else if (ans >= height[i] * (j - i)) break;
+				ans = max(ans, height[l] * (h - l));
+				l++;
 			}
-		}
-		for (int i = n - 1; i >= 0; i--)
-		{
-			for (int j = 0; j < i; j++)
+			else
 			{
-				if (height[j] >= height[i])
-				{
-					ans = max(ans, height[i] * (i - j));
-					break;
-				}
-				else if (ans >= height[i] * (i - j)) break;
+				ans = max(ans, height[h] * (h - l));
+				h--;
 			}
 		}
 		return ans;
