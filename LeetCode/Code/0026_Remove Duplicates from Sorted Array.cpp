0034c2bce6a79d34c001882533d39a6948ab2e89
Kriegcho <1002310519@qq.com>
2022-02-19 11:13:32 +0800

    0026_Remove Duplicates from Sorted Array

---
+++ LeetCode/Code/0026_Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,5 @@
+﻿#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {public:	int removeDuplicates(vector<int>& nums) {		set<int> s;		for (int i = 0; i < nums.size(); i++) if (s.find(nums[i]) == s.end()) s.insert(nums[i]);		set<int>::iterator it = s.begin();		int ind = 0;		while (it != s.end())		{			nums[ind] = *it;			it++;			ind++;		}		return s.size();	}};
