aaddbb2f3290e6d7ce886989355bff4092c4d1e4
Kriegcho <1002310519@qq.com>
2022-02-20 20:06:41 +0800

    0033_Search in Rotated Sorted Array

---
+++ LeetCode/Code/0033_Search in Rotated Sorted Array.cpp
@@ -0,0 +1,48 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		int ns = nums.size(), nf = nums[0];
+		int l = 0, h = ns - 1;
+		if (nf == target) return 0;
+		bool f = target > nums[0];
+		while (l < h)
+		{
+			if (l == h)
+			{
+				if (nums[l] == target) return l;
+				else break;
+			}
+			else if (l + 1 == h)
+			{
+				if (nums[l] == target) return l;
+				if (nums[h] == target) return h;
+				else break;
+			}
+			int mid = (l + h) / 2;
+			if (f)
+			{
+				if (nums[mid] > target) h = mid;
+				else if (nums[mid] < target)
+				{
+					if (nums[mid] > nf) l = mid;
+					else h = mid;
+				}
+				else return mid;
+			}
+			else
+			{
+				if (nums[mid] > target)
+				{
+					if (nums[mid] > nf) l = mid;
+					else h = mid;
+				}
+				else if (nums[mid] < target) l = mid;
+				else return mid;
+			}
+		}
+		return -1;
+	}
+};
