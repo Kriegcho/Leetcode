22c0130d1b314ba27f9d3ce62101a4be2a736ae1
Kriegcho <1002310519@qq.com>
2022-03-11 21:07:22 +0800

    0038_Count and Say

---
+++ LeetCode/0038_Count and Say.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string count(string s)
+	{
+		string ret = "";
+		int len = s.length(), c = 0;
+		char temp = s[0];
+		for (int i = 0; i < len; i++)
+		{
+			if (s[i] == temp) c++;
+			else
+			{
+				ret += to_string(c);
+				ret += temp;
+				temp = s[i];
+				c = 1;
+			}
+		}
+		if (c)
+		{
+			ret += to_string(c);
+			ret += temp;
+		}
+		return ret;
+	}
+
+	string countAndSay(int n) {
+		if (n == 1) return "1";
+		else return count(countAndSay(n - 1));
+	}
+};
