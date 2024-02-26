3fd6bbe515d423f4f1a527b53c011f614409a478
Kriegcho <1002310519@qq.com>
2024-02-26 16:49:01 +0800

    0039_Combination Sum

---
+++ LeetCode/0039_Combination Sum.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+		set<int> cs;
+		for (int i = 0; i < candidates.size(); i++) cs.insert(candidates[i]);
+		set<int>::iterator it = cs.begin(), itr = cs.begin();
+		while (it != cs.end())
+		{
+			it++;
+		}
+	}
+};
