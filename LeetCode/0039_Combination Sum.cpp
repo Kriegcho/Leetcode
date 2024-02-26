42e39b3ba4135669c2e324cd6dac0087dd53a3b0
Kriegcho <1002310519@qq.com>
2024-02-26 17:54:05 +0800

    0039_Combination Sum 覆盖之前的提交错误

--- LeetCode/0039_Combination Sum.cpp
+++ LeetCode/0039_Combination Sum.cpp
@@ -1,16 +1,46 @@
 ﻿#include <vector>
-#include <set>
 using namespace std;
 
 class Solution {
 public:
-	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-		set<int> cs;
-		for (int i = 0; i < candidates.size(); i++) cs.insert(candidates[i]);
-		set<int>::iterator it = cs.begin(), itr = cs.begin();
-		while (it != cs.end())
+	vector<vector<int>> combinationSum(bool* cand, int minc, int target)
+	{
+		if (target < minc) // 剪枝
+			return vector<vector<int>>(0);
+
+		vector<vector<int>> ans;
+		for (int i = minc; i <= target; i++)
 		{
-			it++;
+			if (cand[i])
+			{
+				if (i == target)
+					ans.push_back({ target });
+				else
+				{
+					vector<vector<int>> temp = combinationSum(cand, i, target - i); // 这里将minc改变成i是为了去重
+					for (auto vec : temp)
+					{
+						vec.push_back(i);
+						ans.push_back(vec);
+					}
+				}
+			}
 		}
+
+		return ans;
+	}
+
+	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+		bool cand[41];
+		for (int i = 0; i < 41; i++)
+			cand[i] = false;
+		int minc = target;
+		for (auto c : candidates)
+		{
+			cand[c] = true;
+			if (minc > c)
+				minc = c;
+		}
+		return combinationSum(cand, minc, target);
 	}
 };
