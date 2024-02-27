a9edd0f3e0f81116c068c7d95873db8c4af7cf46
Kriegcho <1002310519@qq.com>
2024-02-27 17:40:41 +0800

    0049_Group Anagrams

---
+++ LeetCode/Code/0049_Group Anagrams.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+	vector<vector<string>> groupAnagrams(vector<string>& strs) {
+		unordered_map<string, int> m;
+		vector<vector<string>> ans;
+		for (string str : strs)
+		{
+			string s(str);
+			sort(s.begin(), s.end());
+			if (m.find(s) == m.end())
+			{
+				m[s] = m.size();
+				ans.resize(m.size());
+			}
+			ans[m[s]].push_back(str);
+		}
+		return ans;
+	}
+};
