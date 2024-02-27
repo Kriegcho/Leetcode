1649d0fbc8726b21e0e56eaac669597500be86b3
Kriegcho <1002310519@qq.com>
2024-02-27 22:31:12 +0800

    0056_Merge Intervals

---
+++ LeetCode/Code/0056_Merge Intervals.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		sort(intervals.begin(), intervals.end());
+		vector<vector<int>> ans;
+		int pos = 0, rval = intervals[0][1], n = intervals.size();
+		for (int i = 0; i < n; i++)
+		{
+			if (rval < intervals[i][0])
+			{
+				ans.push_back({ intervals[pos][0], rval });
+				pos = i;
+				rval = intervals[i][1];
+			}
+			else
+				if (rval < intervals[i][1])
+					rval = intervals[i][1];
+		}
+		ans.push_back({ intervals[pos][0], max(rval, intervals[n - 1][1]) });
+		return ans;
+	}
+};
