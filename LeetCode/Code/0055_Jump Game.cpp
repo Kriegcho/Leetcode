7002ce83b52c796b07b659c59ab0cb37a7fe97b8
Kriegcho <1002310519@qq.com>
2024-02-27 21:40:57 +0800

    0055_Jump Game

---
+++ LeetCode/Code/0055_Jump Game.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+
+using namespace std; 
+
+class Solution {
+public:
+	bool canJump(vector<int>& nums) {
+		int size = nums.size(), pos = size - 1;
+		while (pos > 0)
+		{
+			int newpos = pos;
+			bool find = false;
+			for (int i = pos; i >= 0; i--)
+			{
+				if (nums[i] >= pos - i)
+				{
+					newpos = i;
+					find = true;
+				}
+			}
+			if (!find)
+				return false;
+			pos = newpos;
+		}
+		return true;
+	}
+};
