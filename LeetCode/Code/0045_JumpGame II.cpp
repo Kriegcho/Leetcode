2eedf7da658b52210755032810c2b23b221ea3fb
Kriegcho <1002310519@qq.com>
2024-02-27 16:13:37 +0800

    0045_JumpGame II

---
+++ LeetCode/Code/0045_JumpGame II.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int jump(vector<int>& nums) {
+		int ans = 0, size = nums.size(), pos = size - 1;
+		while (pos > 0)
+		{
+			int newpos = pos;
+			for (int i = pos; i >= 0; i--)
+			{
+				if (nums[i] >= pos - i)
+					newpos = i;
+			}
+			pos = newpos;
+			ans++;
+		}
+		return ans;
+	}
+};
