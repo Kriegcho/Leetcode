d072c6d72849ea5b5b21d9104e860378834b62c6
Kriegcho <1002310519@qq.com>
2024-02-28 17:02:36 +0800

    0513_ Find Bottom Left Tree Value

---
+++ LeetCode/Code/0513_ Find Bottom Left Tree Value.cpp
@@ -0,0 +1,30 @@
+﻿#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+	void solve(TreeNode* root, int level, vector<int>& ans)
+	{
+		if (root == nullptr)
+			return;
+		if (ans.size() < level)
+			ans.push_back(root->val);
+		solve(root->left, level + 1, ans);
+		solve(root->right, level + 1, ans);
+	}
+public:
+	int findBottomLeftValue(TreeNode* root) {
+		vector<int> ans;
+		solve(root, 1, ans);
+		return ans.back();
+	}
+};
