21a9a9d9948102f994629eec2c72e136dd57d8c2
Kriegcho <1002310519@qq.com>
2024-02-28 17:18:53 +0800

    0074_Search a 2D Matrix

---
+++ LeetCode/Code/0074_Search a 2D Matrix.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		int m = matrix.size(), n = matrix[0].size();
+		int l = 0, r = m * n - 1, mid = (l + r) / 2;
+		while (l <= r)
+		{
+			if (matrix[mid / n][mid % n] == target)
+				return true;
+			else if (mid == l)
+			{
+				if (matrix[r / n][r % n] == target)
+					return true;
+				return false;
+			}
+			else if (matrix[mid / n][mid % n] < target)
+				l = mid;
+			else if (matrix[mid / n][mid % n] > target)
+				r = mid;
+			mid = (l + r) / 2;
+		}
+		return false;
+	}
+};
