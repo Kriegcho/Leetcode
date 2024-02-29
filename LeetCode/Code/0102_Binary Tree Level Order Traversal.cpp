ffb9c474c952d40339643104d82da17b63ee5ef5
Kriegcho <1002310519@qq.com>
2024-02-29 19:07:14 +0800

    0102_Binary Tree Level Order Traversal

---
+++ LeetCode/Code/0102_Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+	void solve(TreeNode* node, vector<vector<int>>& ans, int level)
+	{
+		if (node == nullptr)
+			return;
+		int n = ans.size();
+		if (n < level)
+			ans.push_back({ node->val });
+		else
+			ans[level - 1].push_back(node->val);
+		solve(node->left, ans, level + 1);
+		solve(node->right, ans, level + 1);
+	}
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> ans;
+		solve(root, ans, 1);
+		return ans;
+	}
+};
