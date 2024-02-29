e80730cb7e1daceed94b6668e7c61bc3172e3ca5
Kriegcho <1002310519@qq.com>
2024-02-29 15:33:01 +0800

    0078_Subsets

---
+++ LeetCode/Code/0078_Subsets.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+
+using namespace std; 
+
+class Solution {
+	void solve(vector<int>& nums, int r, vector<vector<int>>& ans)
+	{
+		if (r == 0)
+		{
+			ans.push_back({});
+			ans.push_back({ nums[r] });
+		}
+		else
+		{
+			solve(nums, r - 1, ans);
+			int n = ans.size();
+			for (int i = 0; i < n; i++)
+			{
+				auto v1(ans[i]);
+				v1.push_back(nums[r]);
+				ans.push_back(v1);
+			}
+		}
+	}
+public:
+	vector<vector<int>> subsets(vector<int>& nums) {
+		vector<vector<int>> ans;
+		int n = nums.size() - 1;
+		solve(nums, n, ans);
+		return ans;
+	}
+};
