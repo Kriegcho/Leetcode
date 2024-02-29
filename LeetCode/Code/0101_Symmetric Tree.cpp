23918d65374fe2c7a9e2d5ac5db19813000ad884
Kriegcho <1002310519@qq.com>
2024-02-29 18:57:30 +0800

    0101_Symmetric Tree

---
+++ LeetCode/Code/0101_Symmetric Tree.cpp
@@ -0,0 +1,23 @@
+﻿struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+	bool solve(TreeNode* l, TreeNode* r)
+	{
+		if (l == nullptr && r == nullptr)
+			return true;
+		else if (l != nullptr && r != nullptr)
+			return l->val == r->val && solve(l->left, r->right) && solve(l->right, r->left);
+		return false;
+	}
+public:
+	bool isSymmetric(TreeNode* root) {
+		return solve(root->left, root->right);
+	}
+};
