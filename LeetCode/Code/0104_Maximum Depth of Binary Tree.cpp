548ae437965e0a2f26c23ef6852cde8c138e3687
Kriegcho <1002310519@qq.com>
2024-03-01 14:18:57 +0800

    0104_Maximum Depth of Binary Tree

---
+++ LeetCode/Code/0104_Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,26 @@
+﻿struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+	void solve(TreeNode* node, int level, int& ans)
+	{
+		if (node == nullptr)
+			return;
+		if (ans < level)
+			ans = level;
+		solve(node->left, level + 1, ans);
+		solve(node->right, level + 1, ans);
+	}
+public:
+	int maxDepth(TreeNode* root) {
+		int ans = 0;
+		solve(root, 1, ans);
+		return ans;
+	}
+};
