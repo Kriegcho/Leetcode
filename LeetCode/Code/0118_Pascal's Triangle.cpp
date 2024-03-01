942e83c268c1a150c5bf7692db9caf6b570e48d6
Kriegcho <1002310519@qq.com>
2024-03-01 16:26:04 +0800

    0118_Pascal's Triangle

---
+++ LeetCode/Code/0118_Pascal's Triangle.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> ans{ {1} };
+		for (int l = 1; l < numRows; l++)
+		{
+			vector<int> vec;
+			vec.push_back(1);
+			for (int i = 0; i < l - 1; i++)
+			{
+				vec.push_back(ans[l - 1][i] + ans[l - 1][i + 1]);
+			}
+			vec.push_back(1);
+			ans.push_back(vec);
+		}
+		return ans;
+	}
+};
