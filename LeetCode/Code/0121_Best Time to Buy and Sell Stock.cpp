9b3ce3820eb58a787bf7271d93b9c23951b47576
Kriegcho <1002310519@qq.com>
2024-03-01 16:30:26 +0800

    0121_Best Time to Buy and Sell Stock

---
+++ LeetCode/Code/0121_Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int buy = 0, ans = 0, n = prices.size();
+		for (int i = 1; i < n; i++)
+		{
+			if (prices[i] < prices[buy])
+				buy = i;
+			else
+			{
+				int p = prices[i] - prices[buy];
+				if (ans < p)
+					ans = p;
+			}
+		}
+		return ans;
+	}
+};
