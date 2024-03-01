69a9b28a30fc2544d1bb18ff5323fa0d5862c1e7
Kriegcho <1002310519@qq.com>
2024-03-01 14:28:13 +0800

    2864_Maximum Odd Binary Number

---
+++ LeetCode/Code/2864_Maximum Odd Binary Number.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	string maximumOddBinaryNumber(string s) {
+		int pos = -1, len = s.length();
+		for (int i = 0; i < len - 1; i++)
+			if (s[i] == '1')
+			{
+				if (pos == -1)
+				{
+					swap(s[i], s[len - 1]);
+					i--;
+				}
+				else
+					swap(s[pos], s[i]);
+				pos++;
+			}
+		return s;
+	}
+};
