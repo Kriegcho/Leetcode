4c92b30d1a8417720cab151fb8a08f35ab7e9188
Kriegcho <1002310519@qq.com>
2024-03-01 18:13:21 +0800

    0128_Longest Consecutive Sequence

---
+++ LeetCode/Code/0128_Longest Consecutive Sequence.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+	int longestConsecutive(vector<int>& nums) {
+		set<int> s;
+		int n = nums.size();
+		if (n == 0)
+			return 0;
+		for (auto i : nums)
+			s.insert(i);
+		int ans = 1, count = 1, beg = *(s.begin());
+		for (auto i : s)
+		{
+			if (i == beg)
+				continue;
+			else if (i == beg + count)
+			{
+				count++;
+				if (ans < count)
+					ans = count;
+			}
+			else
+			{
+				beg = i;
+				count = 1;
+			}
+		}
+		return ans;
+	}
+};
