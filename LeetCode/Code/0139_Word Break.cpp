2a8e6dacc7414e693e021640e34cc60fb5182142
Kriegcho <1002310519@qq.com>
2024-03-01 23:05:19 +0800

    0139_Word Break

---
+++ LeetCode/Code/0139_Word Break.cpp
@@ -0,0 +1,32 @@
+﻿#include <unordered_map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+	bool DFS(string s, int pos, int n, unordered_map<string, int>& map, vector<bool>& flag)
+	{
+		if (flag[pos] == false)
+			return false;
+		if (pos == n)
+			return true;
+		for (int i = pos; i < n; i++)
+		{
+			if (map[s.substr(pos, i - pos + 1)] == 1)
+				if (DFS(s, i + 1, n, map, flag))
+					return true;
+		}
+		flag[pos] = false;
+		return false;
+	}
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		unordered_map<string, int> map;
+		for (auto s : wordDict)
+			map[s] = 1;
+		int n = s.length();
+		vector<bool> flag(n, true); // 从i开始到结尾能否匹配
+		return DFS(s, 0, n, map, flag);
+	}
+};
