1059bf51c386e935551cb6ab3b43c972f01d6ab8
Kriegcho <1002310519@qq.com>
2024-03-01 22:29:47 +0800

    0163_Single Number

---
+++ LeetCode/Code/0163_Single Number.cpp
@@ -0,0 +1,13 @@
+﻿#include <vector>
+
+using namespace std; 
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int ans = 0, n = nums.size();
+		for (int i = 0; i < n; i++)
+			ans ^= nums[i];
+		return ans;
+	}
+};
