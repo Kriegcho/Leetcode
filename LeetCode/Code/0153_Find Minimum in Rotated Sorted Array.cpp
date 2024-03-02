9bc500806036d6a1663f14c6fe17c9aacbc8155d
Kriegcho <1002310519@qq.com>
2024-03-02 16:09:59 +0800

    0153_Find Minimum in Rotated Sorted Array

---
+++ LeetCode/Code/0153_Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int findMin(vector<int>& nums) {
+		int n = nums.size();
+		int l = 0, r = n - 1;
+		if (nums[l] < nums[r])
+			return nums[l];
+		else
+		{
+			int m = (l + r) / 2;
+			while (l < r)
+			{
+				if (nums[m] < nums[r])
+					r = m;
+				else
+					l = m + 1;
+				m = (l + r) / 2;
+			}
+			return nums[l];
+		}
+	}
+};
