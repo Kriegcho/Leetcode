bdb65bbdb8a03ae7fa38094df3318c5f3ddee04d
Kriegcho <1002310519@qq.com>
2024-03-02 21:23:17 +0800

    0207_Course Schedule 拓扑排序

--- LeetCode/Code/0207_Course Schedule.cpp
+++ LeetCode/Code/0207_Course Schedule.cpp
@@ -4,38 +4,41 @@
 using namespace std;
 
 class Solution {
-	bool DFS(int pos, const vector<vector<int>>& pre, vector<bool> visit, vector<bool>& visited)
-	{
-		visit[pos] = false;
-		for (auto i : pre[pos])
-		{
-			if (visited[i])
-				continue;
-			if (!visit[i])
-				return false;
-			if (!DFS(i, pre, visit, visited))
-				return false;
-		}
-		visit[pos] = true;
-		visited[pos] = true;
-		return true;
-	}
-
 public:
 	bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 		vector<vector<int>> pre(numCourses);
+		vector<int> post(numCourses, 0);
 		for (auto vec : prerequisites)
-			pre[vec[0]].push_back(vec[1]);
-		vector<bool> visit(numCourses, true);
-		vector<bool> visited(numCourses, false); // 标记已经访问过的课程，从这个课程开始向后找都是合法的
-		for (int i = 0; i < numCourses; i++)
 		{
-			if (visit[i] && !pre[i].empty())
-			{
-				if (!DFS(i, pre, visit, visited))
-					return false;
-			}
+			pre[vec[0]].push_back(vec[1]);
+			post[vec[1]]++;
 		}
+		queue<int> q;
+		for (int i = 0; i < numCourses; i++)
+			if (post[i] == 0)
+				q.push(i);
+
+		while (!q.empty())
+		{
+			for (auto j : pre[q.front()])
+			{
+				post[j]--;
+				if (post[j] == 0)
+					q.push(j);
+			}
+			q.pop();
+		}
+				
+		for (auto i : post)
+			if (i != 0)
+				return false;
 		return true;
 	}
 };
+
+int main()
+{
+	Solution s;
+	vector<vector<int>> vec{ {1, 4}, {2,4},{3,1},{3,2} };
+	s.canFinish(5, vec);
+}
