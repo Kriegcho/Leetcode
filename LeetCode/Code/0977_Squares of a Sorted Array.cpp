5c47177a75bc15d4f82c2dbba082e8f42bc5d6d9
Kriegcho <1002310519@qq.com>
2024-03-02 15:33:52 +0800

    0977_Squares of a Sorted Array

---
+++ LeetCode/Code/0977_Squares of a Sorted Array.cpp
@@ -0,0 +1,46 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> sortedSquares(vector<int>& nums) {
+		// 将输入分割为两部分，负值取绝对值，两者合并为一个新数组
+		int n = nums.size(), pos = n;
+		for (int i = 0; i < n; i++)
+			if (nums[i] >= 0)
+			{
+				pos = i;
+				break;
+			}
+		vector<int> ans;
+		int l = pos - 1, r = pos;
+		while (l >= 0 || r < n)
+		{
+			if (l < 0)
+			{
+				ans.push_back(nums[r] * nums[r]);
+				r++;
+			}
+			else if (r >= n)
+			{
+				ans.push_back(nums[l] * nums[l]);
+				l--;
+			}
+			else
+			{
+				if ((-nums[l]) < nums[r])
+				{
+					ans.push_back(nums[l] * nums[l]);
+					l--;
+				}
+				else
+				{
+					ans.push_back(nums[r] * nums[r]);
+					r++;
+				}
+			}
+		}
+		return ans;
+	}
+};
