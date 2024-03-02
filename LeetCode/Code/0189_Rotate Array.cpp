90f95bea318f1c864469eb4aa2828b5aaf60f7e3
Kriegcho <1002310519@qq.com>
2024-03-02 17:21:08 +0800

    0189_Rotate Array

---
+++ LeetCode/Code/0189_Rotate Array.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+	int gcd(int a, int b)
+	{
+		if (a == b)
+			return a;
+		else if (a < b)
+			swap(a, b);
+		return gcd(a - b, b);
+	}
+public:
+	void rotate(vector<int>& nums, int k) {
+		int n = nums.size();
+		k %= n;
+		if (k == 0)
+			return;
+		// 需要执行的次数即为k和n的最大公约数
+		int loop = gcd(k, n);
+		for (int i = 0; i < loop; i++)
+		{
+			int pos = i + k;
+			while (pos != i)
+			{
+				swap(nums[i], nums[pos]);
+				pos = (pos + k) % n;
+			}
+		}
+	}
+};
