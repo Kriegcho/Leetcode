f9418f7b478714cb8edfe45ddb07e8a033220a68
Kriegcho <1002310519@qq.com>
2024-03-02 16:34:34 +0800

    0155_Min Stack

---
+++ LeetCode/Code/0155_Min Stack.cpp
@@ -0,0 +1,31 @@
+﻿#include <stack>
+#include <map>
+
+class MinStack {
+	std::map<int, int> map;
+	std::stack<int> st;
+public:
+	MinStack() {
+
+	}
+
+	void push(int val) {
+		map[val]++;
+		st.push(val);
+	}
+
+	void pop() {
+		if (map[st.top()] == 1)
+			map.erase(st.top());
+		else map[st.top()]--;
+		st.pop();
+	}
+
+	int top() {
+		return st.top();
+	}
+
+	int getMin() {
+		return map.begin()->first;
+	}
+};
