433083990ea0961b86c66b9602338b8c5662a7b4
Kriegcho <1002310519@qq.com>
2024-03-02 19:53:44 +0800

    0199_Binary Tree Right Side View

---
+++ LeetCode/Code/0199_Binary Tree Right Side View.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+	void solve(TreeNode* node, int level, vector<int>& ans)
+	{
+		if (node == nullptr)
+			return;
+		if (ans.size() < level)
+			ans.push_back(node->val);
+		else
+			ans[level - 1] = node->val;
+		solve(node->left, level + 1, ans);
+		solve(node->right, level + 1, ans);
+	}
+public:
+	vector<int> rightSideView(TreeNode* root) {
+		vector<int> ans;
+		solve(root, 1, ans);
+		return ans;
+	}
+};
