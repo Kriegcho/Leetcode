5b544e20985b24339777ae297f3b6c0b05da9ce4
Kriegcho <1002310519@qq.com>
2024-03-02 16:51:52 +0800

    0169_Majority Element

---
+++ LeetCode/Code/0169_Majority Element.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int cur = nums[0], count = 0;
+		for (auto i : nums)
+		{
+			if (i == cur)
+				count++;
+			else
+			{
+				count--;
+				if (count < 0)
+				{
+					cur = i;
+					count = 1;
+				}
+			}
+		}
+		return cur;
+	}
+};
