4330592377ecbf2bceafff3ba05969f1fb149812
Kriegcho <1002310519@qq.com>
2024-03-03 16:27:38 +0800

    0287_Find the Duplicate Number

---
+++ LeetCode/Code/0287_Find the Duplicate Number.cpp
@@ -0,0 +1,17 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int findDuplicate(vector<int>& nums) {
+		int n = nums.size();
+		while (1)
+		{
+			if (nums[0] == nums[nums[0]])
+				break;
+			swap(nums[0], nums[nums[0]]);
+		}
+		return nums[0];
+	}
+};
