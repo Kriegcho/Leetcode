1881191db2b6e73ac4602b48980a1baeacb3f659
Kriegcho <1002310519@qq.com>
2024-03-03 13:49:33 +0800

    0238_Product of Array Except Self

---
+++ LeetCode/Code/0238_Product of Array Except Self.cpp
@@ -0,0 +1,35 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> productExceptSelf(vector<int>& nums) {
+		int p_0 = 1, c_0 = 0; // 非0积，0数量
+		for (auto i : nums)
+		{
+			if (i != 0)
+				p_0 *= i;
+			else
+				c_0++;
+		}
+		for (auto& i : nums)
+		{
+			switch (c_0)
+			{
+			case 0:
+				i = p_0 / i;
+				break;
+			case 1:
+				if (i == 0)
+					i = p_0;
+				else
+					i = 0;
+				break;
+			default:
+				i = 0;
+			}
+		}
+		return nums;
+	}
+};
