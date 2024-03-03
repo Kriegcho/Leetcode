ba3bab3aaded7d9df27250966af6a5abffc9d1eb
Kriegcho <1002310519@qq.com>
2024-03-03 14:44:22 +0800

    0239_Sliding Window Maximum

---
+++ LeetCode/Code/0239_Sliding Window Maximum.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+		priority_queue<pair<int, int>> pq;
+		int n = nums.size();
+		vector<int> ans;
+		for (int i = 0; i < n; i++)
+		{
+			pq.push(make_pair(nums[i], i));
+			if (i >= k - 1)
+			{
+				while (pq.top().second < i - k + 1)
+					pq.pop();
+				ans.push_back(pq.top().first);
+			}
+		}
+		return ans;
+	}
+};
