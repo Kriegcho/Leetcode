3d7e191066f3b1b49a12995467c06abc0638345a
Kriegcho <1002310519@qq.com>
2024-03-03 16:57:46 +0800

    0295_Find Median from Data Stream

---
+++ LeetCode/Code/0295_Find Median from Data Stream.cpp
@@ -0,0 +1,36 @@
+﻿#include <set>
+
+class MedianFinder {
+	std::multiset<int> s;
+	std::multiset<int>::iterator it;
+public:
+	MedianFinder() {
+		it = s.end();
+	}
+
+	void addNum(int num) {
+		s.insert(num);
+		if (it == s.end())
+			it = s.begin();
+		else
+		{
+			int n = s.size();
+			if (n % 2 == 1 && num >= *it)
+				it++;
+			if (n % 2 == 0 && num < *it)
+				it--;
+		}
+	}
+
+	double findMedian() {
+		int n = s.size();
+		if (n % 2 == 0)
+		{
+			double ans = 1.0 * (*it + *++it) / 2;
+			it--;
+			return ans;
+		}
+		else
+			return *it;
+	}
+};
