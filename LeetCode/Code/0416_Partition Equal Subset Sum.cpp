b36276b25384eccfe60a77c67b869556c70f28e9
Kriegcho <1002310519@qq.com>
2024-03-03 22:48:18 +0800

    0416_Partition Equal Subset Sum

---
+++ LeetCode/Code/0416_Partition Equal Subset Sum.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool canPartition(vector<int>& nums) {
+		int sum = 0;
+		for (auto i : nums)
+			sum += i;
+		if (sum % 2 != 0)
+			return false;
+
+		int n = nums.size();
+		int t = sum / 2;
+		vector<bool> dp(t + 1, false); // 是否能组合成j
+		if (nums[0] <= t)
+			dp[nums[0]] = true;
+		dp[0] = true;
+		for (int i = 1; i < n; i++)
+			for (int j = t; j > 0; j--)
+				if (nums[i] <= j)
+					dp[j] = dp[j] | dp[j - nums[i]];
+		return dp[t];
+	}
+};
