3e4b6df6109ccd3e11f951a56756ff5a9b07206c
Kriegcho <1002310519@qq.com>
2024-03-03 19:22:46 +0800

    0300_Longest Increasing Subsequence

---
+++ LeetCode/Code/0300_Longest Increasing Subsequence.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> dp(n, 1); // 以nums[i]为子序列结尾的最长递增子序列长度
+		for (int i = 1; i < n; i++)
+			for (int j = 0; j < i; j++)
+				if (nums[j] < nums[i])
+					dp[i] = max(dp[i], dp[j] + 1);
+		int ans = 1;
+		for (auto i : dp)
+			if (ans < i)
+				ans = i;
+		return ans;
+	}
+};
