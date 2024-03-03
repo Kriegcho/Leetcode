1a7bf4137fbdada1ef63207a0c2e0a13345f4cbd
Kriegcho <1002310519@qq.com>
2024-03-03 23:04:42 +0800

    0438_Find All Anagrams in a String

---
+++ LeetCode/Code/0438_Find All Anagrams in a String.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> findAnagrams(string s, string p) {
+		unordered_map<char, int> mapp;
+		for (auto c : p)
+			mapp[c]++;
+		int ls = s.length(), lp = p.length();
+		vector<int> ans;
+		for (int i = 0; i < ls; i++)
+		{
+			mapp[s[i]]--;
+			if (mapp[s[i]] == 0)
+				mapp.erase(s[i]);
+			if (mapp.empty())
+				ans.push_back(i - lp + 1);
+
+			if (i >= lp)
+			{
+				mapp[s[i - lp]]++;
+				if (mapp[s[i - lp]] == 0)
+					mapp.erase(s[i - lp]);
+				if (mapp.empty())
+					ans.push_back(i - lp + 1);
+			}
+		}
+		return ans;
+	}
+};
