f1063acef8611f6ae0ae13420bbf1533940dbb2d
Kriegcho <1002310519@qq.com>
2024-03-03 16:08:19 +0800

    0283_Move Zeroes

---
+++ LeetCode/Code/0283_Move Zeroes.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+		int pos = -1, n = nums.size();
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] == 0)
+			{
+				if (pos == -1)
+					pos = i;
+			}
+			else if(pos != -1)
+			{
+				swap(nums[i], nums[pos]);
+				pos++;
+			}
+		}
+	}
+};
