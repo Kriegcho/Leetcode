b3d0f5efb59eddc3623d8d6d3bccbc08b737d387
Kriegcho <1002310519@qq.com>
2024-03-03 15:11:32 +0800

    0239_Sliding Window Maximum, deque

--- LeetCode/Code/0239_Sliding Window Maximum.cpp
+++ LeetCode/Code/0239_Sliding Window Maximum.cpp
@@ -1,23 +1,30 @@
 ﻿#include <vector>
-#include <queue>
+#include <deque>
 
 using namespace std;
 
 class Solution {
 public:
 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-		priority_queue<pair<int, int>> pq;
+		deque<int> dq;
 		int n = nums.size();
 		vector<int> ans;
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < k - 1; i++)
 		{
-			pq.push(make_pair(nums[i], i));
-			if (i >= k - 1)
-			{
-				while (pq.top().second < i - k + 1)
-					pq.pop();
-				ans.push_back(pq.top().first);
-			}
+			while (!dq.empty() && dq.back() < nums[i])
+				dq.pop_back();
+			dq.push_back(nums[i]);
+		}
+		for (int i = k - 1; i < n; i++)
+		{
+			while (!dq.empty() && dq.back() < nums[i])
+				dq.pop_back();
+			dq.push_back(nums[i]);
+
+			ans.push_back(dq.front());
+
+			if (dq.front() == nums[i - k + 1])
+				dq.pop_front();
 		}
 		return ans;
 	}
