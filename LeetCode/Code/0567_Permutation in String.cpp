98cc1f02526c49d43d5ec471eedb7fe316b9b9b7
Kriegcho <1002310519@qq.com>
2024-03-04 13:38:31 +0800

    0567_Permutation in String

---
+++ LeetCode/Code/0567_Permutation in String.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <unordered_map>
+
+using namespace std; 
+
+class Solution {
+public:
+	bool checkInclusion(string s1, string s2) {
+		int l1 = s1.length(), l2 = s2.length();
+		if (l1 > l2)
+			return false;
+		unordered_map<char, int> map;
+		for (auto c : s1)
+			map[c]++;
+		for (int i = 0; i < l2; i++)
+		{
+			map[s2[i]]--;
+			if (map[s2[i]] == 0)
+				map.erase(s2[i]);
+			if (map.empty())
+				return true;
+			if (i >= l1)
+			{
+				map[s2[i - l1]]++;
+				if (map[s2[i - l1]] == 0)
+					map.erase(s2[i - l1]);
+				if (map.empty())
+					return true;
+			}
+		}
+		return false;
+	}
+};
