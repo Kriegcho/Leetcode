c63944810b89d06fc7ff0d160cfe0ed6460af242
Kriegcho <1002310519@qq.com>
2024-03-04 13:42:14 +0800

    0704_Binary Search

---
+++ LeetCode/Code/0704_Binary Search.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+
+using namespace std; 
+
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		int n = nums.size(), l = 0, r = n - 1, m = (l + r) / 2;
+		while (l <= r)
+		{
+			if (nums[m] == target)
+				return m;
+			else if (nums[m] < target)
+				l = m + 1;
+			else
+				r = m - 1;
+			m = (l + r) / 2;
+		}
+		return -1;
+	}
+};
