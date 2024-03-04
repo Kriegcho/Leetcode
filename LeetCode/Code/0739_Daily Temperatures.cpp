0b38c90c72b77c1ba606c869466b3de51df51c5d
Kriegcho <1002310519@qq.com>
2024-03-04 14:23:46 +0800

    0739_Daily Temperatures

---
+++ LeetCode/Code/0739_Daily Temperatures.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> dailyTemperatures(vector<int>& temperatures) {
+		int n = temperatures.size();
+		vector<int> ans(n, 0);
+		stack<int> st;
+		for (int i = n - 1; i >= 0; i--)
+		{
+			while (!st.empty() && temperatures[i] >= temperatures[st.top()])
+				st.pop();
+			ans[i] = st.empty() ? 0 : st.top() - i;
+			st.push(i);
+		}
+		return ans;
+	}
+};
