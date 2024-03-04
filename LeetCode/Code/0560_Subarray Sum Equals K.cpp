ffae2b4e274edef3c983c960368fab35f9063bbb
Kriegcho <1002310519@qq.com>
2024-03-04 13:38:22 +0800

    0560_Subarray Sum Equals K

---
+++ LeetCode/Code/0560_Subarray Sum Equals K.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	int subarraySum(vector<int>& nums, int k) {
+		int n = nums.size();
+		unordered_map<int, int> m;
+		int sum = 0, ans = 0; // sum表示从0到i的和
+		m[0] = 1;
+		for (auto i : nums)
+		{
+			sum += i;
+			if (m.find(sum - k) != m.end()) // sum(0 ... i) - sum (0 ... j) == k,说明(i, j]是所求的一个解
+				ans += m[sum - k];
+			m[sum]++;
+		}
+		return ans;
+	}
+};
