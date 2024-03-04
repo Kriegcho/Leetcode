86c1b6cb324b07bc071fbadfbc70d7a3ae4d90fa
Kriegcho <1002310519@qq.com>
2024-03-04 15:43:54 +0800

    0948_Bag of Tokens, O(n)。时间复杂度比上个版本要好一些，但是LeetCode上面提交的结果反而差一些。。。提交仅作记录

--- LeetCode/Code/0948_Bag of Tokens.cpp
+++ LeetCode/Code/0948_Bag of Tokens.cpp
@@ -4,22 +4,6 @@
 using namespace std;
 
 class Solution {
-	int binarySearch(const vector<int>& vec, int target) // 返回第一个大于target的下标
-	{
-		int n = vec.size();
-		int l = 0, r = n - 1, m = (l + r) / 2;
-		while (l <= r)
-		{
-			if (vec[m] > target)
-				r = m;
-			else
-				l = m + 1;
-			if (m == l)
-				break;
-			m = (l + r) / 2;
-		}
-		return l;
-	}
 public:
 	int bagOfTokensScore(vector<int>& tokens, int power) {
 		int n = tokens.size();
@@ -28,19 +12,16 @@ public:
 		sort(tokens.begin(), tokens.end());
 		if (tokens[0] > power)
 			return 0;
-		vector<int> vsum(n);
-		int sum = 0;
-		for (int i = 0; i < n; i++)
+		int ans = 0, use = 0;
+		for (int i = 0; i < n - use; i++)
 		{
-			sum += tokens[i];
-			vsum[i] = sum;
-		}
-		int ans = binarySearch(vsum, power);
-		for (int i = n - 1; i >= n / 2; i--)
-		{
-			power += tokens[i];
-			int ind = binarySearch(vsum, power);
-			ans = max(ans, ind - (n - i));
+			power -= tokens[i];
+			if (power < 0)
+			{
+				use++;
+				power += tokens[n - use];
+			}
+			ans = max(i + 1 - use, ans);
 		}
 		return ans;
 	}
