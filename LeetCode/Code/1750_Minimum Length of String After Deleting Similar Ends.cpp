8887f8831f8d9a7e7808dfde47e9f84170374dbf
Kriegcho <1002310519@qq.com>
2024-03-05 17:35:41 +0800

    1750_Minimum Length of String After Deleting Similar Ends

---
+++ LeetCode/Code/1750_Minimum Length of String After Deleting Similar Ends.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	int minimumLength(string s) {
+		char c = 'd';
+		int len = s.length(), l = 0, r = len - 1;
+		while (l < r)
+		{
+			if (s[l] == s[r])
+			{
+				c = s[l];
+				l++;
+				r--;
+			}
+			else
+			{
+				if (s[l] == c)
+					l++;
+				else if (s[r] == c)
+					r--;
+				else
+					break;
+			}
+		}
+		if (l == r && s[l] == c)
+			return 0;
+		return r - l + 1;
+	}
+};
