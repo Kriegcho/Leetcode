7d6970a0fa099457262c46f69d059de6c332e01e
Kriegcho <1002310519@qq.com>
2024-03-07 17:16:36 +0800

    0876_Middle of the Linked List

---
+++ LeetCode/Code/0876_Middle of the Linked List.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	ListNode * middleNode(ListNode* head) {
+		std::vector<ListNode*> vec;
+		int n = 0;
+		ListNode* node = head;
+		while (node != 0)
+		{
+			n++;
+			vec.push_back(node);
+			node = node->next;
+		}
+		return vec[n / 2];
+	}
+};
