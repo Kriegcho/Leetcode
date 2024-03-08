f57955b7c954bbe460073932596fcf039869ce3c
Kriegcho <1002310519@qq.com>
2024-03-08 17:49:09 +0800

    3005_Count Elements With Maximum Frequency

---
+++ LeetCode/Code/3005_Count Elements With Maximum Frequency.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxFrequencyElements(vector<int>& nums) {
+		unordered_map<int, int> um;
+		int c = 0, ans = 0;
+		for (auto i : nums)
+		{
+			um[i]++;
+			if (c < um[i])
+			{
+				c = um[i];
+				ans = c;
+			}
+			else if (c == um[i])
+				ans += um[i];
+		}
+		return ans;
+	}
+};
