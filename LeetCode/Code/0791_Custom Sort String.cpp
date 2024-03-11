5d58266c06fa75621c370a20587e09fd3b3c7a4a
Kriegcho <1002310519@qq.com>
2024-03-11 17:03:29 +0800

    0791_Custom Sort String

---
+++ LeetCode/Code/0791_Custom Sort String.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	string customSortString(string order, string s) {
+		unordered_map<char, int> um;
+		for (auto c : s)
+			um[c]++;
+		string ans;
+		for (auto c : order)
+			while (um[c] > 0)
+			{
+				ans += c;
+				um[c]--;
+			}
+		for (auto p : um)
+			while (p.second > 0)
+			{
+				ans += p.first;
+				p.second--;
+			}
+		return ans;
+	}
+};
