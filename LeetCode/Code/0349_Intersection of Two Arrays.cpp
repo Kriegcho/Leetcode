f9d07fc1882e5b4496e101eab2cc796a690c38b4
Kriegcho <1002310519@qq.com>
2024-03-11 17:06:04 +0800

    0349_Intersection of Two Arrays

---
+++ LeetCode/Code/0349_Intersection of Two Arrays.cpp
@@ -0,0 +1,21 @@
+﻿#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+		unordered_map<int, int> um;
+		for (auto i : nums1)
+			um[i]++;
+		vector<int> ans;
+		for (auto i : nums2)
+			if (um[i] > 0)
+			{
+				ans.push_back(i);
+				um[i] = 0;
+			}
+		return ans;
+	}
+};
