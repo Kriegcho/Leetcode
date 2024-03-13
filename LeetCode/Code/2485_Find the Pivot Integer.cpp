30ba7d2d370dbcd1068809a3ce78d64a92bad5bc
Kriegcho <1002310519@qq.com>
2024-03-13 19:51:13 +0800

    2485_Find the Pivot Integer

---
+++ LeetCode/Code/2485_Find the Pivot Integer.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+	int pivotInteger(int n) {
+		int l = n / 2, r = n;
+		int m = (l + r) / 2;
+		int s = n * (n + 1) / 2;
+		while (l <= r)
+		{
+			if (m * (m + 1) == s + m)
+				return m;
+			else if (m * (m + 1) < s + m)
+				l = l + 1;
+			else
+				r = r - 1;
+			m = (l + r) / 2;
+		}
+		return -1;
+	}
+};
