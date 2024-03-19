66eeadffff4b2edbaaaea9f1e6d89452b6841b2e
Kriegcho <1002310519@qq.com>
2024-03-19 15:49:19 +0800

    0452_Minimum Number of Arrows to Burst Balloons

---
+++ LeetCode/Code/0452_Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std; 
+
+class Solution {
+public:
+	int findMinArrowShots(vector<vector<int>>& points) {
+		sort(points.begin(), points.end());
+
+		int ans = 1;
+		int pos = points[0][1];
+		for (auto v : points)
+		{
+			if (v[0] > pos)
+			{
+				ans++;
+				pos = v[1];
+			}
+			else if (v[1] < pos)
+				pos = v[1];
+		}
+		return ans;
+	}
+};
