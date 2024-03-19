5d11e366cefe58cd6ede3dca7f1a6a32a0a19431
Kriegcho <1002310519@qq.com>
2024-03-19 13:55:27 +0800

    0621_Task Scheduler

---
+++ LeetCode/Code/0621_Task Scheduler.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int leastInterval(vector<char>& tasks, int n) {
+		vector<int> vec(26, 0);
+		int maxc = 0;
+		for (auto c : tasks)
+		{
+			vec[c - 'A']++;
+			if (maxc < vec[c - 'A'])
+				maxc = vec[c - 'A'];
+		}
+		int ans = (maxc - 1) * (n + 1);
+		for (auto i : vec)
+			if (i == maxc)
+				ans++;
+		// 剩余的插进去即可，不会破坏规则
+		if (ans < tasks.size())
+			ans = tasks.size();
+		return ans;
+	}
+};
