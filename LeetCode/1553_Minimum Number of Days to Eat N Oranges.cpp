df30b959d42a97402ffc44ce63d932dce39e2618
Kriegcho <1002310519@qq.com>
2024-03-29 17:01:44 +0800

    1553_Minimum Number of Days to Eat N Oranges

---
+++ LeetCode/1553_Minimum Number of Days to Eat N Oranges.cpp
@@ -0,0 +1,21 @@
+﻿#include <unordered_map>
+
+using namespace std; 
+
+class Solution {
+	unordered_map<int, int> um;
+public:
+	int minDays(int n) {
+		if (n <= 1)
+			um[n] = n;
+		else if (!um.count(n))
+		{
+			int ans = minDays(n / 3) + n % 3 + 1;
+			int t = minDays(n / 2) + n % 2 + 1;
+			if (ans > t)
+				ans = t;
+			um[n] = ans;
+		}
+		return um[n];
+	}
+};
