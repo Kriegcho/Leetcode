4bf2dda6b9141601295f92e9575a2c44657c5529
Kriegcho <1002310519@qq.com>
2024-03-29 16:06:20 +0800

    2962_Count Subarrays Where Max Element Appears at Least K Times

---
+++ LeetCode/2962_Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -0,0 +1,35 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	long long countSubarrays(vector<int>& nums, int k) {
+		int maxn = nums[0], cnt = 0;
+		vector<int> pos;
+		int n = nums.size();
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] == maxn)
+			{
+				cnt++;
+				pos.push_back(i);
+			}
+			else if (nums[i] > maxn)
+			{
+				maxn = nums[i];
+				cnt = 1;
+				pos.clear();
+				pos.push_back(i);
+			}
+		}
+		if (pos.size() < k)
+			return 0;
+		long long ans = 0;
+		int np = pos.size();
+		pos.push_back(n);
+		for (int i = 0; i <= np - k; i++)
+			ans += (long long)(pos[i] + 1) * (long long)(pos[i + k - 1 + 1] - pos[i + k - 1]);
+		return ans;
+	}
+};
