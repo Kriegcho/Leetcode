3eb1c9174b8cbce91219ecf09bead5ccfba0428f
Kriegcho <1002310519@qq.com>
2024-04-09 17:30:34 +0800

    2073_Time Needed to Buy Tickets

---
+++ LeetCode/2073_Time Needed to Buy Tickets.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int timeRequiredToBuy(vector<int>& tickets, int k) {
+		int n = tickets.size(), sum = n;
+		vector<int> count(101, 0);
+		for (int i : tickets)
+			count[i]++;
+		int ans = 0, last = 0;
+		for (int i = 0; i < tickets[k]; i++)
+		{
+			if (count[i] > 0)
+			{
+				ans += sum * (i - last);
+				sum -= count[i];
+				last = i;
+			}
+		}
+		int cnt = 0;
+		for (int i = 0; i <= k; i++)
+			if (tickets[i] >= tickets[k])
+				cnt++;
+		ans += sum * (tickets[k] - last - 1) + cnt;
+		return ans;
+	}
+};
