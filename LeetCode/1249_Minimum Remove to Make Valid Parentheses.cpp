8141461315f79cd6e7cd776e866fe2e54ce98a57
Kriegcho <1002310519@qq.com>
2024-04-09 17:30:48 +0800

    1249_Minimum Remove to Make Valid Parentheses

---
+++ LeetCode/1249_Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+	string minRemoveToMakeValid(string s) {
+		string ans;
+		int cnt = 0, len = s.length();
+		for (int i = 0; i < len; i++)
+		{
+			if (s[i] == '(')
+				cnt++;
+			else if (s[i] == ')')
+			{
+				if (cnt == 0)
+					continue;
+				else
+					cnt--;
+			}
+			ans += s[i];
+		}
+
+		if (cnt > 0)
+		{
+			len = ans.length();
+			for (int i = len - 1; i >= 0; i--)
+			{
+				if (ans[i] == '(')
+				{
+					ans.erase(ans.begin() + i);
+					cnt--;
+					if (cnt == 0)
+						break;
+				}
+			}
+		}
+
+		return ans;
+	}
+};
