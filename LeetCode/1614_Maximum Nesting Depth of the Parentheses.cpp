f9143b1392d69f8ee341b1b683f545dd73c8db4d
Kriegcho <1002310519@qq.com>
2024-04-10 12:59:43 +0800

    1614_Maximum Nesting Depth of the Parentheses

---
+++ LeetCode/1614_Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int maxDepth(string s) {
+		int ans = 0;
+		int cnt = 0;
+		for (char c : s)
+		{
+			if (c == '(')
+			{
+				cnt++;
+				if (cnt > ans)
+					ans = cnt;
+			}
+			else if (c == ')')
+				cnt--;
+		}
+		return ans;
+	}
+};
