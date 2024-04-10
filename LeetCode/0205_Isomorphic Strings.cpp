cb18f94475b15a53f45cae8b3fe9b0eca4271dca
Kriegcho <1002310519@qq.com>
2024-04-10 13:23:01 +0800

    0205_Isomorphic Strings

---
+++ LeetCode/0205_Isomorphic Strings.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <unordered_map>
+using namespace std; 
+
+class Solution {
+public:
+	bool isIsomorphic(string s, string t) {
+		int len = s.length();
+		unordered_map<char, char> mts, mst;
+		for (int i = 0; i < len; i++)
+		{
+			if (mts.count(t[i]) && mst.count(s[i]))
+			{
+				if (mts[t[i]] != s[i])
+					return false;
+			}
+			else if (mts.count(t[i]) || mst.count(s[i]))
+				return false;
+			else
+			{
+				mts[t[i]] = s[i];
+				mst[s[i]] = t[i];
+			}
+		}
+		return true;
+	}
+};
