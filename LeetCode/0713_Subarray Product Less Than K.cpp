549cbd6b0449dc70d9d1d584107bc677862084cd
Kriegcho <1002310519@qq.com>
2024-04-10 17:27:57 +0800

    0713_Subarray Product Less Than K

---
+++ LeetCode/0713_Subarray Product Less Than K.cpp
@@ -0,0 +1,38 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+		int ans = 0;
+		int multi = nums[0];
+		int l = 0, r = 1, n = nums.size();
+		while (l <= r)
+		{
+			if (multi < k)
+			{
+				if (r == n)
+					break;
+				multi *= nums[r];
+				r++;
+			}
+			else
+			{
+				if (r > l)
+					ans += r - l - 1;
+				multi /= nums[l];
+				l++;
+			}
+		}
+		if (multi < k && l < r && r == n)
+			ans += (r - l + 1) * (r - l) / 2;
+		return ans;
+	}
+};
+
+int main()
+{
+	vector<int> vec{10,5,2,6};
+	Solution s;
+	s.numSubarrayProductLessThanK(vec, 100);
+}
