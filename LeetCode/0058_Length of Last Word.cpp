5a338eaece4c191348b01bdaee5e160f4bfec7e2
Kriegcho <1002310519@qq.com>
2024-04-10 13:29:49 +0800

    0058_Length of Last Word

---
+++ LeetCode/0058_Length of Last Word.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		int len = s.length();
+		int pos = -1;
+		int beg = 0;
+		for (int i = len - 1; i >= 0; i--)
+		{
+			if (pos == -1 && s[i] != ' ')
+				pos = i;
+			else if (pos != -1 && s[i] == ' ')
+			{
+				beg = i;
+				break;
+			}
+		}
+		if (beg == 0 && s[beg] != ' ')
+			beg--;
+		return pos - beg;
+	}
+};
