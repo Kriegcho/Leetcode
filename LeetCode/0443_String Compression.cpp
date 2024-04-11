74bdf33f61156fbdabaa1ad9478d0891b9f3f150
Kriegcho <1002310519@qq.com>
2024-04-11 22:37:33 +0800

    0443_String Compression

---
+++ LeetCode/0443_String Compression.cpp
@@ -0,0 +1,71 @@
+﻿#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+	int compress(vector<char>& chars) {
+		int ans = 0;
+		char last = ' ';
+		int cnt = 0;
+		for (char c : chars)
+		{
+			if (last == ' ')
+			{
+				last = c;
+				cnt = 1;
+			}
+			else if (c == last)
+				cnt++;
+			else
+			{
+				chars[ans++] = last;
+				if (cnt > 1)
+				{
+					if (cnt < 10)
+						chars[ans++] = '0' + cnt;
+					else
+					{
+						stack<int> s;
+						s.push(cnt % 10);
+						while (cnt > 10)
+						{
+							cnt /= 10;
+							s.push(cnt % 10);
+						}
+						while (!s.empty())
+						{
+							chars[ans++] = '0' + s.top();
+							s.pop();
+						}
+					}
+				}
+
+				last = c;
+				cnt = 1;
+			}
+		}
+		chars[ans++] = last;
+		if (cnt > 1)
+		{
+			if (cnt < 10)
+				chars[ans++] = '0' + cnt;
+			else
+			{
+				stack<int> s;
+				s.push(cnt % 10);
+				while (cnt > 10)
+				{
+					cnt /= 10;
+					s.push(cnt % 10);
+				}
+				while (!s.empty())
+				{
+					chars[ans++] = '0' + s.top();
+					s.pop();
+				}
+			}
+		}
+		return ans;
+	}
+};
