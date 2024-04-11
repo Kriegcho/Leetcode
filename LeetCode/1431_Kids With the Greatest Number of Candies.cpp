2032ff2aa332ebf126280e0662a6522b5cbe68d8
Kriegcho <1002310519@qq.com>
2024-04-11 21:52:59 +0800

    1431_Kids With the Greatest Number of Candies.cpp

---
+++ LeetCode/1431_Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+		int max = 0;
+		for (int i : candies)
+			if (i > max)
+				max = i;
+		vector<bool> ans(candies.size());
+		for (int i = 0; i < candies.size(); i++)
+			ans[i] = candies[i] + extraCandies >= max;
+		return ans;
+	}
+};
