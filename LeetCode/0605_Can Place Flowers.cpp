50efa19df7f1ec28702c47faa5d5ef7cdaedfbde
Kriegcho <1002310519@qq.com>
2024-04-11 21:58:15 +0800

    0605_Can Place Flowers

---
+++ LeetCode/0605_Can Place Flowers.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+		int ans = 0, len = flowerbed.size();
+		if (n > len / 2 + 1)
+			return false;
+		for (int i = 0; i < len; i++)
+			if (flowerbed[i] == 0)
+			{
+				bool flag = true;
+				if (i > 0)
+					flag &= flowerbed[i - 1] == 0;
+				if (i < len - 1)
+					flag &= flowerbed[i + 1] == 0;
+				if (flag)
+				{
+					ans++;
+					flowerbed[i] = 1;
+				}
+			}
+		return ans >= n;
+	}
+};
