fd65207b9475c24e1eb00637d1388486a897320a
Kriegcho <1002310519@qq.com>
2024-04-11 22:20:36 +0800

    0334_Increasing Triplet Subsequence

---
+++ LeetCode/0334_Increasing Triplet Subsequence.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool increasingTriplet(vector<int>& nums) {
+		int n = nums.size();
+		int mincurmax = INT_MAX, mincurmin = INT_MAX;
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] > mincurmax)
+				return true;
+			if (mincurmin > nums[i])
+				mincurmin = nums[i];
+			if (nums[i] > mincurmin && mincurmax > nums[i])
+				mincurmax = nums[i];
+		}
+		return false;
+	}
+};
