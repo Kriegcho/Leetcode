92cdc2a9571ce066f8b5388b8a22f7291fca678d
Kriegcho <1002310519@qq.com>
2024-04-12 11:33:04 +0800

    0392_Is Subsequence.cpp

---
+++ LeetCode/0392_Is Subsequence.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+using namespace std; 
+
+class Solution {
+public:
+	bool isSubsequence(string s, string t) {
+		int si = 0, ti = 0, sl = s.length(), tl = t.length();
+		while (si < sl && ti < tl)
+		{
+			if (s[si] == t[ti])
+			{
+				si++;
+				ti++;
+			}
+			else
+				ti++;
+		}
+		return (si == sl);
+	}
+};
