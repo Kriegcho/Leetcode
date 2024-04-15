f0f346d3151e5dd18cc2a9e5748ba11d779b76e9
Kriegcho <1002310519@qq.com>
2024-04-16 00:04:15 +0800

    0404_Sum of Left Leaves

---
+++ LeetCode/0404_Sum of Left Leaves.cpp
@@ -0,0 +1,33 @@
+﻿struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+	void solve(TreeNode* node, int& ans, bool left)
+	{
+		if (!node)
+			return;
+		if (!node->left && !node->right)
+		{
+			if (left)
+				ans += node->val;
+		}
+		else
+		{
+			solve(node->left, ans, true);
+			solve(node->right, ans, false);
+		}
+	}
+public:
+	int sumOfLeftLeaves(TreeNode* root) {
+		int ans = 0;
+		bool left = false;
+		solve(root, ans, left);
+		return ans;
+	}
+};
