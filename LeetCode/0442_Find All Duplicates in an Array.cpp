04d172a2623ee1d2f2aa09c74dd8da31f103e6f3
Kriegcho <1002310519@qq.com>
2024-04-16 21:13:52 +0800

    0442_Find All Duplicates in an Array

---
+++ LeetCode/0442_Find All Duplicates in an Array.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	vector<int> findDuplicates(vector<int>& nums) {
+		int n = nums.size();
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] == -1)
+				continue;
+			if (nums[i] != i + 1)
+			{
+				if (nums[nums[i] - 1] == -1 || nums[i] == nums[nums[i] - 1])
+					nums[nums[i] - 1] = -1;
+				else
+				{
+					swap(nums[i], nums[nums[i] - 1]);
+					i--;
+				}
+			}
+		}
+		vector<int> ans;
+		for (int i = 0; i < n; i++)
+			if (nums[i] == -1)
+				ans.push_back(i + 1);
+		return ans;
+	}
+};
