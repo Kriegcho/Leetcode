5de5f193c7758da1b31759b54962197739457a36
Kriegcho <1002310519@qq.com>
2024-04-16 22:09:37 +0800

    1456_Maximum Number of Vowels in a Substring of Given Length

---
+++ LeetCode/1456_Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+	int solve(char c)
+	{
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+			return 1;
+		else
+			return 0;
+	}
+public:
+	int maxVowels(string s, int k) {
+		int sum = 0;
+		vector<int> vec;
+		for (char c : s)
+			vec.push_back(solve(c));
+		for (int i = 0; i < k; i++)
+			sum += vec[i];
+		if (sum == k)
+			return k;
+		int ans = sum;
+		for (int i = k; i < vec.size(); i++)
+		{
+			sum += vec[i];
+			sum -= vec[i - k];
+			if (sum == k)
+				return k;
+			if (sum > ans)
+				ans = sum;
+		}
+		return ans;
+	}
+};
