713c25b5713068ce642e6f826857638a52db36dd
Kriegcho <1002310519@qq.com>
2024-04-16 22:09:22 +0800

    0643_Maximum Average Subarray I

---
+++ LeetCode/0643_Maximum Average Subarray I.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	double findMaxAverage(vector<int>& nums, int k) {
+		int sum = 0;
+		for (int i = 0; i < k; i++)
+			sum += nums[i];
+		int max = sum;
+		for (int i = k; i < nums.size(); i++)
+		{
+			sum += nums[i];
+			sum -= nums[i - k];
+			if (sum > max)
+				max = sum;
+		}
+		double ans = 1.0 * max / k;
+		return ans;
+	}
+};
