390be20da232cb35054156272654825373925502
Kriegcho <1002310519@qq.com>
2024-04-16 22:35:33 +0800

    1493_Longest Subarray of 1's After Deleting One Element

---
+++ LeetCode/1493_Longest Subarray of 1's After Deleting One Element.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	int longestSubarray(vector<int>& nums) {
+		int l = 0, r = 0, n = nums.size();
+		bool flag = true;
+		int ans = 0;
+		while (r < n)
+		{
+			if (flag)
+			{
+				if (nums[r] == 0)
+					flag = false;
+				r++;
+				if (r - l > ans)
+					ans = r - l;
+			}
+			else
+			{
+				if (nums[r] == 0)
+				{
+					while (!flag)
+					{
+						if (nums[l] == 0)
+							flag = true;
+						l++;
+					}
+				}
+				else
+				{
+					r++;
+					if (r - l > ans)
+						ans = r - l;
+				}
+			}
+		}
+		ans--;
+		return ans;
+	}
+};
