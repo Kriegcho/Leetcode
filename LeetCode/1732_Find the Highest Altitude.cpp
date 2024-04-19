f6b3fe1c68db97b648ef43512dc5b45f8ce17ec5
Kriegcho <1002310519@qq.com>
2024-04-19 10:42:03 +0800

    1732_Find the Highest Altitude.cpp

---
+++ LeetCode/1732_Find the Highest Altitude.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int largestAltitude(vector<int>& gain) {
+		int sum = 0, ans = 0;
+		for (int i : gain)
+		{
+			sum += i;
+			if (ans < sum)
+				ans = sum;
+		}
+		return ans;
+	}
+};
