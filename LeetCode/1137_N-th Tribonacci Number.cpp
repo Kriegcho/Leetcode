38f03501b94bd5ed26d5ca9227cada68c8f6f61b
Kriegcho <1002310519@qq.com>
2024-04-27 23:16:24 +0800

    1137_N-th Tribonacci Number.cpp

---
+++ LeetCode/1137_N-th Tribonacci Number.cpp
@@ -0,0 +1,19 @@
+﻿#include <unordered_map>
+
+class Solution {
+	std::unordered_map<int, int> um;
+public:
+	int tribonacci(int n) {
+		if (n == 0)
+			um[n] = 0;
+		else if (n <= 2)
+			um[n] = 1;
+		else
+		{
+			um[n] = (um.count(n - 1) ? um[n - 1] : tribonacci(n - 1))
+				+ (um.count(n - 2) ? um[n - 2] : tribonacci(n - 2))
+				+ (um.count(n - 3) ? um[n - 3] : tribonacci(n - 3));
+		}
+		return um[n];
+	}
+};
