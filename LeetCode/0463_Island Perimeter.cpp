7f6e9891c37057adadc4cb9d67a76e9160d77636
Kriegcho <1002310519@qq.com>
2024-04-27 23:16:03 +0800

    0463_Island Perimeter.cpp

---
+++ LeetCode/0463_Island Perimeter.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	int islandPerimeter(vector<vector<int>>& grid) {
+		int ans = 0;
+		int m = grid.size(), n = grid[0].size();
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				if (grid[i][j] == 1)
+				{
+					ans += 4;
+					if (i > 0 && grid[i - 1][j] == 1)
+						ans--;
+					if (i < m - 1 && grid[i + 1][j] == 1)
+						ans--;
+					if (j > 0 && grid[i][j - 1] == 1)
+						ans--;
+					if (j < n - 1 && grid[i][j + 1] == 1)
+						ans--;
+				}
+		return ans;
+	}
+};
