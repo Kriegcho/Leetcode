37ce1a3a54dad51c5f09ec1cad848c067e18d286
Kriegcho <1002310519@qq.com>
2024-08-02 15:13:59 +0800

    2134_Minimum Swaps to Group All 1's Together II.cpp

---
+++ LeetCode/2134_Minimum Swaps to Group All 1's Together II.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int minSwaps(vector<int>& nums) {
+		int k = 0;
+		for (int n : nums)
+			if (n == 1) k++;
+		int cur = 0;
+		for (int i = 0; i < k; i++)
+			if (nums[i] == 0) cur++;
+		int ans = cur;
+		for (int i = k; i < k + nums.size(); i++)
+		{
+			if (nums[i % (nums.size())] == 0) cur++;
+			if (nums[i - k] == 0) cur--;
+			if (ans > cur) ans = cur;
+		}
+		return ans;
+	}
+};
