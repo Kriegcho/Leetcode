e7d5c59bd1e5b92e4de74772f15e77b80a5153e2
Kriegcho <1002310519@qq.com>
2024-08-05 17:00:50 +0800

    0274_H-Index.cpp

---
+++ LeetCode/0274_H-Index.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int hIndex(vector<int>& citations) {
+		vector<int> cnt(citations.size() + 1, 0);
+		for (int n : citations)
+			cnt[min(n,(int)citations.size())]++;
+		int sum = 0;
+		for (int i = citations.size(); i >= 0; i--)
+		{
+			sum += cnt[i];
+			if (sum >= i)
+				return i;
+		}
+		return 0;
+	}
+};
