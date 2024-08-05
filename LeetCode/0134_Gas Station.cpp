3f4fbc07c6dd89d4f636d6bc2486c98d559f4e19
Kriegcho <1002310519@qq.com>
2024-08-05 17:00:37 +0800

    0134_Gas Station.cpp

---
+++ LeetCode/0134_Gas Station.cpp
@@ -0,0 +1,30 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+		vector<int> left(gas.size());
+		int sum = 0;
+		for (int i = 0; i < gas.size(); i++)
+		{
+			left[i] = gas[i] - cost[i];
+			sum += left[i];
+		}
+		if (sum < 0) return -1;
+		sum = 0;
+		int ans = -1;
+		for (int i = 0; i < left.size(); i++)
+		{
+			sum += left[i];
+			if (sum < 0)
+			{
+				ans = -1;
+				sum = 0;
+			}
+			else if (ans == -1)
+				ans = i;
+		}
+		return ans;
+	}
+};
