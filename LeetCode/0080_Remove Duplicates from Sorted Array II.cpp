8c7c5cc4c692080a569b157a2729efe4807f5aab
Kriegcho <1002310519@qq.com>
2024-08-05 17:00:25 +0800

    0080_Remove Duplicates from Sorted Array II.cpp

---
+++ LeetCode/0080_Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int ind = 0;
+		int cur = nums[0];
+		int cur_cnt = -1;
+		for (int n : nums)
+		{
+			if (n == cur)
+			{
+				cur_cnt++;
+				if (cur_cnt < 2)
+					nums[ind++] = n;
+			}
+			else
+			{
+				cur = n;
+				cur_cnt = 0;
+				nums[ind++] = n;
+			}
+		}
+		return ind;
+	}
+};
