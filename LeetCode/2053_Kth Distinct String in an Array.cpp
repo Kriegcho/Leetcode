98e4058100d0b673b100df88628c26f5fa825317
Kriegcho <1002310519@qq.com>
2024-08-05 17:01:18 +0800

    2053_Kth Distinct String in an Array.cpp

---
+++ LeetCode/2053_Kth Distinct String in an Array.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+	string kthDistinct(vector<string>& arr, int k) {
+		unordered_map<string, int> um;
+		for (const auto& s : arr)
+			um[s]++;
+		int ind = 0;
+		for (const auto& s : arr)
+			if (um[s] == 1)
+			{
+				ind++;
+				if (ind == k)
+					return s;
+			}
+		return "";
+	}
+};
