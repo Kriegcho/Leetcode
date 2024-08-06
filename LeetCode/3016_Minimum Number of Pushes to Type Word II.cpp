63d795e26a3f5f74ef4a74632d99cadbb4f96abc
Kriegcho <1002310519@qq.com>
2024-08-06 09:47:29 +0800

    3016_Minimum Number of Pushes to Type Word II.cpp

---
+++ LeetCode/3016_Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minimumPushes(string word) {
+		vector<int> vec(26, 0);
+		for (char c : word)
+			vec[c - 'a']++;
+		sort(vec.begin(), vec.end(), [](int a, int b)->bool {return a > b; });
+		int sum = 0;
+		for (int i = 0; i < 26; i++)
+		{
+			sum += vec[i] * (i / 8 + 1);
+		}
+		return sum;
+	}
+};
