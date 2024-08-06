ef26289214701ffe4b79fed495fafdb6e9c1914c
Kriegcho <1002310519@qq.com>
2024-08-06 15:55:01 +0800

    1209_Minimum Size Subarray Sum.cpp

---
+++ LeetCode/0209_Minimum Size Subarray Sum.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minSubArrayLen(int target, vector<int>& nums) {
+		int l = 0, r = 0;
+		int sum = nums[0];
+		while (sum < target && r < nums.size() - 1)
+		{
+			r++;
+			sum += nums[r];
+		}
+		if (sum < target) return 0;
+		int ans = r - l + 1;
+		while (r < nums.size())
+		{
+			if (sum >= target)
+			{
+				ans = min(ans, r - l + 1);
+				sum -= nums[l];
+				l++;
+			}
+			else if (r < nums.size() - 1)
+			{
+				r++;
+				sum += nums[r];
+			}
+			else break;
+		}
+		return ans;
+	}
+};
