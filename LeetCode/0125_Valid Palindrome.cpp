8bed5d11f6e50d16e5cafd46ee4f4b3a28ef8d34
Kriegcho <1002310519@qq.com>
2024-08-06 10:59:49 +0800

    0125_Valid Palindrome.cpp

---
+++ LeetCode/0125_Valid Palindrome.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+	bool isAlphanumeric(char c)
+	{
+		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
+	}
+	char convert(char c)
+	{
+		if (c >= 'A' && c <= 'Z')
+			return c - 'A' + 'a';
+		else return c;
+	}
+public:
+	bool isPalindrome(string s) {
+		string ns;
+		for (char c : s)
+			if (isAlphanumeric(c)) ns += convert(c);
+		int l = 0, r = ns.length() - 1;
+		while (l <= r)
+		{
+			if (ns[l] != ns[r]) return false;
+			l++;
+			r--;
+		}
+		return true;
+	}
+};
