85b08a61f9b39cbbb97e77abfea9dd299211c53b
Kriegcho <1002310519@qq.com>
2024-08-07 10:30:07 +0800

    0242_Valid Anagram.cpp

---
+++ LeetCode/0242_Valid Anagram.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool isAnagram(string s, string t) {
+		if (s.length() != t.length()) return false;
+		vector<int> vec(26, 0);
+		for (char c : s)
+			vec[c - 'a']++;
+		for (char c : t)
+		{
+			vec[c - 'a']--;
+			if (vec[c - 'a'] < 0) return false;
+		}
+		return true;
+	}
+};
