3ef3583af357f17c738e7e42c017813d3dd54028
Kriegcho <1002310519@qq.com>
2024-08-07 10:38:40 +0800

    0202_Happy Number.cpp

---
+++ LeetCode/0202_Happy Number.cpp
@@ -0,0 +1,28 @@
+﻿#include <unordered_set>
+
+class Solution {
+	int calc(int n)
+	{
+		int res = 0;
+		while (n)
+		{
+			int t = n % 10;
+			res += t * t;
+			n /= 10;
+		}
+		return res;
+	}
+public:
+
+	bool isHappy(int n) {
+		std::unordered_set<int> s;
+		int t = calc(n);
+		while (t != 1)
+		{
+			if (s.count(t)) return false;
+			s.insert(t);
+			t = calc(t);
+		}
+		return true;
+	}
+};
