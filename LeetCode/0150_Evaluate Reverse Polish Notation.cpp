89f40842072df2e8b123e39d3a80f64939d04f0e
Kriegcho <1002310519@qq.com>
2024-08-08 10:25:54 +0800

    0150_Evaluate Reverse Polish Notation.cpp

---
+++ LeetCode/0150_Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,51 @@
+﻿#include <vector>
+#include <string>
+using namespace std; 
+
+class Solution {
+public:
+	int evalRPN(vector<string>& tokens) {
+		vector<int> vec;
+		for (string s : tokens)
+		{
+			if (s == "+")
+			{
+				int op1 = vec.back();
+				vec.pop_back();
+				int op2 = vec.back();
+				vec.pop_back();
+				vec.push_back(op2 + op1);
+			}
+			else if (s == "-")
+			{
+				int op1 = vec.back();
+				vec.pop_back();
+				int op2 = vec.back();
+				vec.pop_back();
+				vec.push_back(op2 - op1);
+			}
+			else if (s == "*")
+			{
+				int op1 = vec.back();
+				vec.pop_back();
+				int op2 = vec.back();
+				vec.pop_back();
+				vec.push_back(op2 * op1);
+			}
+			else if (s == "/")
+			{
+				int op1 = vec.back();
+				vec.pop_back();
+				int op2 = vec.back();
+				vec.pop_back();
+				vec.push_back(op2 / op1);
+			}
+			else
+			{
+				int i = stoi(s);
+				vec.push_back(i);
+			}
+		}
+		return vec.back();
+	}
+};
