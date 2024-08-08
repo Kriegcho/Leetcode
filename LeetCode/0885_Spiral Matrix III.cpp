96af76b454a32f85444509b3956e8972ab600304
Kriegcho <1002310519@qq.com>
2024-08-08 09:57:24 +0800

    0885_Spiral Matrix III.cpp

---
+++ LeetCode/0885_Spiral Matrix III.cpp
@@ -0,0 +1,49 @@
+﻿#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+	vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+		vector<vector<int>> ans;
+		int edge = 1, total = rows * cols;
+		auto solve = [&ans, rows, cols, total](int r, int c)->bool {
+			if (r >= 0 && r < rows && c >= 0 && c < cols)
+			{
+				ans.push_back({ r, c });
+				return ans.size() == total;
+			}
+			return false;
+		};
+		while (1) {
+			rStart++;
+			for (int i = 0; i < edge; i++)
+			{
+				rStart--;
+				if (solve(rStart, cStart)) return ans;
+			}
+
+			for (int i = 0; i < edge; i++)
+			{
+				cStart++;
+				if (solve(rStart, cStart)) return ans;
+
+			}
+
+			for (int i = 0; i < edge; i++)
+			{
+				rStart++;
+				if (solve(rStart, cStart)) return ans;
+
+			}
+
+			for (int i = 0; i < edge; i++)
+			{
+				cStart--;
+				if (solve(rStart, cStart)) return ans;
+
+			}
+			cStart--;
+			edge += 2;
+		}
+	}
+};
