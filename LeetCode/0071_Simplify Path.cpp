ff15eb76bdcf8b21760edd75ff59bf2dc2c15e50
Kriegcho <1002310519@qq.com>
2024-08-08 10:14:55 +0800

    0071_Simplify Path.cpp

---
+++ LeetCode/0071_Simplify Path.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	string simplifyPath(string path) {
+		vector<string> vec;
+		string tmp;
+		for (char c : path)
+		{
+			if (c == '/')
+			{
+				if (!tmp.empty())
+				{
+					if (tmp == "/..")
+					{
+						if (!vec.empty()) vec.pop_back();
+					}
+					else if (tmp != "/." && tmp != "/")
+						vec.push_back(tmp);
+					tmp.clear();
+					
+				}
+				tmp += c;
+			}
+			else
+				tmp += c;
+		}
+		if (tmp == "/..")
+		{
+			if (!vec.empty()) vec.pop_back();
+		}
+		else if (tmp != "/." && tmp != "/")
+			vec.push_back(tmp);
+
+		string ans;
+		for (auto s : vec)
+			ans += s;
+		if (ans.empty()) ans = "/";
+		return ans;
+	}
+};
